ceed2356b4b68d4a0e80b1544d1f1ed863dda337
talopesp <talopescontato@gmail.com>
2025-05-29 12:51:38 -0300

    Corrige porta para ambiente Render

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser');
 const path = require('path');
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 // Configurações
 app.use(bodyParser.urlencoded({ extended: true }));
